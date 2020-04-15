ec93133fa406784af422582143de229e0bfb1d14
Max Rottenkolber <max@mr.gy>
2020-04-15 16:00:51 +0200

    x86 spare regs

--- src/checksum.rs
+++ src/checksum.rs
@@ -61,86 +61,82 @@ pub fn ipsum(data: &[u8], length: usize, initial: u16) -> u16 {
 
 #[cfg(target_arch="x86_64")]
 unsafe fn checksum(data: &[u8], length: usize, initial: u16) -> u16 {
-    let ptr: *const u8 = data.as_ptr();
-    let initial = initial as u64;
-    let csum: u16;
+    let mut _ptr = data.as_ptr();
+    let mut _size = length;
+    let mut acc = initial as u64;
     asm!("
 .intel_syntax noprefix;
 # Accumulative sum.
-mov rax, rdx                # Dx (3rd argument: initial).
-xchg al, ah                 # Swap to convert to host-bytes order.
-mov rcx, rsi                # Rsi (2nd argument: size).
-xor r9, r9                  # Clear out r9. Stores value of array.
-xor r8, r8                  # Clear out r8. Stores array index.
+xchg al, ah               # Swap to convert to host-bytes order.
 1:
-cmp rcx, 32                 # If index is less than 32.
-jl 2f                       # Jump to branch '2'.
-add rax, [rdi + r8]         # Sum acc with qword[0].
-adc rax, [rdi + r8 + 8]     # Sum with carry qword[1].
-adc rax, [rdi + r8 + 16]    # Sum with carry qword[2].
-adc rax, [rdi + r8 + 24]    # Sum with carry qword[3]
-adc rax, 0                  # Sum carry-bit into acc.
-sub rcx, 32                 # Decrease index by 8.
-add r8, 32                  # Jump two qwords.
-jmp 1b                      # Go to beginning of loop.
+cmp rcx, 32               # If index is less than 32.
+jl 2f                     # Jump to branch '2'.
+add rax, [rdi]            # Sum acc with qword[0].
+adc rax, [rdi + 8]        # Sum with carry qword[1].
+adc rax, [rdi + 16]       # Sum with carry qword[2].
+adc rax, [rdi + 24]       # Sum with carry qword[3]
+adc rax, 0                # Sum carry-bit into acc.
+sub rcx, 32               # Decrease index by 8.
+add rdi, 32               # Jump two qwords.
+jmp 1b                    # Go to beginning of loop.
 2:
-cmp rcx, 16                 # If index is less than 16.
-jl 3f                       # Jump to branch '3'.
-add rax, [rdi + r8]         # Sum acc with qword[0].
-adc rax, [rdi + r8 + 8]     # Sum with carry qword[1].
-adc rax, 0                  # Sum carry-bit into acc.
-sub rcx, 16                 # Decrease index by 8.
-add r8, 16                  # Jump two qwords.
+cmp rcx, 16               # If index is less than 16.
+jl 3f                     # Jump to branch '3'.
+add rax, [rdi]            # Sum acc with qword[0].
+adc rax, [rdi + 8]        # Sum with carry qword[1].
+adc rax, 0                # Sum carry-bit into acc.
+sub rcx, 16               # Decrease index by 8.
+add rdi, 16               # Jump two qwords.
 3:
-cmp rcx, 8                  # If index is less than 8.
-jl 4f                       # Jump to branch '4'.
-add rax, [rdi + r8]         # Sum acc with qword[0].
-adc rax, 0                  # Sum carry-bit into acc.
-sub rcx, 8                  # Decrease index by 8.
-add r8, 8                   # Next 64-bit.
+cmp rcx, 8                # If index is less than 8.
+jl 4f                     # Jump to branch '4'.
+add rax, [rdi]            # Sum acc with qword[0].
+adc rax, 0                # Sum carry-bit into acc.
+sub rcx, 8                # Decrease index by 8.
+add rdi, 8                # Next 64-bit.
 4:
-cmp rcx, 4                  # If index is less than 4.
-jl 5f                       # Jump to branch '5'.
-mov r9d, dword ptr [rdi+r8] # Fetch 32-bit from data + r8 into r9d.
-add rax, r9                 # Sum acc with r9. Accumulate carry.
-sub rcx, 4                  # Decrease index by 4.
-add r8, 4                   # Next 32-bit.
+cmp rcx, 4                # If index is less than 4.
+jl 5f                     # Jump to branch '5'.
+mov esi, dword ptr [rdi]  # Fetch 32-bit into rsi.
+add rax, rsi              # Sum acc with rsi. Accumulate carry.
+sub rcx, 4                # Decrease index by 4.
+add rdi, 4                # Next 32-bit.
 5:
-cmp rcx, 2                  # If index is less than 2.
-jl 6f                       # Jump to branch '6'.
-movzx r9, word ptr [rdi+r8] # Fetch 16-bit from data + r8 into r9.
-add rax, r9                 # Sum acc with r9. Accumulate carry.
-sub rcx, 2                  # Decrease index by 2.
-add r8, 2                   # Next 16-bit.
+cmp rcx, 2                # If index is less than 2.
+jl 6f                     # Jump to branch '6'.
+movzx rsi, word ptr [rdi] # Fetch 16-bit into rsi.
+add rax, rsi              # Sum acc with rsi. Accumulate carry.
+sub rcx, 2                # Decrease index by 2.
+add rdi, 2                # Next 16-bit.
 6:
-cmp rcx, 1                  # If index is less than 1.
-jl 7f                       # Jump to branch '7'.
-movzx r9, byte ptr [rdi+r8] # Fetch 8-bit from data + r8 into r9.
-add rax, r9                 # Sum acc with r9. Accumulate carry.
+cmp rcx, 1                # If index is less than 1.
+jl 7f                     # Jump to branch '7'.
+movzx rsi, byte ptr [rdi] # Fetch 8-bit into rsi.
+add rax, rsi              # Sum acc with rsi. Accumulate carry.
 # Fold 64-bit into 16-bit.
 7:
-mov r9, rax                 # Assign acc to r9.
-shr r9, 32                  # Shift r9 32-bit. Stores higher part of acc.
-mov eax, eax                # Clear out higher-part of rax. Stores lower part of acc.
-add eax, r9d                # 32-bit sum of acc and r9.
-adc eax, 0                  # Sum carry to acc.
-mov r9d, eax                # Repeat for 16-bit.
-shr r9d, 16
+mov rsi, rax              # Assign acc to rsi.
+shr rsi, 32               # Shift rsi 32-bit. Stores higher part of acc.
+mov eax, eax              # Clear out higher-part of rax. Stores lower part of acc.
+add eax, esi              # 32-bit sum of acc and rsi.
+adc eax, 0                # Sum carry to acc.
+mov esi, eax              # Repeat for 16-bit.
+shr esi, 16
 and eax, 0x0000ffff
-add ax, r9w
+add ax, si
 adc ax, 0
 # One's complement.
-not eax                     # One-complement of eax.
-and eax, 0xffff             # Clear out higher part of eax.
+not eax                   # One-complement of eax.
+and eax, 0xffff           # Clear out higher part of eax.
 # Swap.
 xchg al, ah
 "
-         :/* outputs */ "={ax}"(csum)
-         :/* inputs */ "{rdi}"(ptr), "{rsi}"(length), "{rdx}"(initial)
-         :/* clobbers */ "rcx", "r8", "r9"
+         :/* outputs */ "={ax}"(acc), "={rdi}"(_ptr), "={rcx}"(_size)
+         :/* inputs */ "0"(acc), "1"(_ptr), "2"(_size)
+         :/* clobbers */ "rsi"
          :/* options */ "volatile"
     );
-    csum
+    acc as u16
 }
 
 #[cfg(target_arch="aarch64")]
